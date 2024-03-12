5e14abf734046c91c23dffadafb790778bbb6383
santhosh <santhoshsanthosh63268@gmail.com>
2024-03-12 18:38:40 +0530

    firstcommit

--- First Assignment/script.js
+++ First Assignment/script.js
@@ -13,19 +13,3 @@ for (let i = 0; i < tabHeaderElement.length; i++) {
     tabbodyElements[i].classList.add("active");
   });
 }
-
-// const tab = document.querySelector(".tab_form");
-// const tabHeader = document.querySelector(".tab_header");
-// const tabbody = document.querySelector(".tab_body");
-
-// tabHeader.addEventListener("click", function (event) {
-//   if (event.target.tagName === "DIV") {
-//     const clickedTabHeader = event.target;
-//     // console.log(clickedTabHeader);
-//     const index = Array.from(tabHeader.children).indexOf(clickedTabHeader);
-//     tabHeader.querySelector(".active").classList.remove("active");
-//     clickedTabHeader.classList.add("active");
-//     tabbody.querySelector(".active").classList.remove("active");
-//     tabbody.children[index].classList.add("active");
-//   }
-// });
